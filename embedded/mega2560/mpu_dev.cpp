520e7b4a6ccfb568b818bc0c7ca89d2c0b8ba620
Felix <f.goosens@student.vu.nl>
2024-04-27 15:32:53 +0200

    Updated MPU library and added calibration on init

--- embedded/mega2560/mpu_dev.cpp
+++ embedded/mega2560/mpu_dev.cpp
@@ -11,30 +11,73 @@ int icompare(const void* a, const void* b){
 int mpu_dev_class::init(){
 
 	if(!this->bmp.begin()){
+		this->bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Operating Mode.
+			Adafruit_BMP280::SAMPLING_X2,     // Temp. oversampling
+			Adafruit_BMP280::SAMPLING_X16,    // Pressure oversampling
+			Adafruit_BMP280::FILTER_X16,      // Filtering.
+			Adafruit_BMP280::STANDBY_MS_500); // Standby time
+	}
+
+	Wire.begin();
+	Wire.setClock(400000);
+
+	/* I2C bus,  0x68 address */
+	this->imu.Config(&Wire, bfs::Mpu9250::I2C_ADDR_PRIM);
+
+	/* Initialize and configure IMU */
+	if (!this->imu.Begin()) {
+		Serial.println("Error initializing communication with IMU");
 		return -1;
 	}
-	this->bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Operating Mode.
-		Adafruit_BMP280::SAMPLING_X2,     // Temp. oversampling
-		Adafruit_BMP280::SAMPLING_X16,    // Pressure oversampling
-		Adafruit_BMP280::FILTER_X16,      // Filtering.
-		Adafruit_BMP280::STANDBY_MS_500); // Standby time
-//	return this->mpu.setup(MPU9250_DEFAULT_ADDRESS);
-	this->mpu.setMagneticDeclination(2.16);
-	return this->mpu.setup(0x68);
+
+	// Sample rate is 1000 / (SRD + 1) = 1000 Hz
+	if (!this->imu.ConfigSrd(0)) {
+		Serial.println("Error configured SRD");
+		return -1;
+	}
+	// 2.16 is the magnetic declination in Amsterdam
+	this->calibrate(2.16);
+
+	return 0;
 }
 
 void mpu_dev_class::calibrate(float magnetic_declination){
-	this->mpu.setMagneticDeclination(magnetic_declination);
-    this->mpu.calibrateAccelGyro();
-    this->mpu.calibrateMag();
+	struct vec_struct radps_avg;
+	struct vec_struct mps2_avg;
+	int samples = 1000;
+	for(int i=0; i<samples; i++){
+		delay(10);
+		this->update();
+		radps_avg.x += this->radps.x;
+		radps_avg.y += this->radps.y;
+		radps_avg.z += this->radps.z;
+
+		mps2_avg.x += this->mps2.x;
+		mps2_avg.y += this->mps2.y;
+		mps2_avg.z += this->mps2.z;
+	}
+	this->radps_offsets.x = radps_avg.x / samples;
+	this->radps_offsets.y = radps_avg.y / samples;
+	this->radps_offsets.z = radps_avg.z / samples;
+
+	this->mps2_offsets.x = mps2_avg.x / samples;
+	this->mps2_offsets.y = mps2_avg.y / samples;
+	this->mps2_offsets.z = mps2_avg.z / samples;
 }
 
 void mpu_dev_class::update(){
+	if(imu.Read()){
+		imu.new_imu_data();
+		imu.new_mag_data();
+
+		this->radps.x = imu.gyro_x_radps() - this->radps_offsets.x;
+		this->radps.y = imu.gyro_y_radps() - this->radps_offsets.y;
+		this->radps.z = imu.gyro_z_radps() - this->radps_offsets.z;
+
+		this->mps2.x = imu.accel_x_mps2() - this->mps2_offsets.x;
+		this->mps2.y = imu.accel_y_mps2() - this->mps2_offsets.y;
+		this->mps2.z = imu.accel_z_mps2() - this->mps2_offsets.z;
+	}
 	this->temp = this->bmp.readTemperature();
 	this->pressure = this->bmp.readPressure();
-
-	this->mpu.update();
-	this->yaw = this->mpu.getYaw();
-	this->pitch = this->mpu.getPitch();
-	this->roll = this->mpu.getRoll();
 }
--- embedded/mega2560/mpu_dev.h
+++ embedded/mega2560/mpu_dev.h
@@ -1,18 +1,28 @@
 #pragma once
 #include <serial_comm.h>
 #include <Adafruit_BMP280.h>
-#include <MPU9250.h>
+#include <math.h>
+#include "mpu9250.h"
+
+struct vec_struct {
+	float x = 0;
+	float y = 0;
+	float z = 0;
+};
 
 class mpu_dev_class{
 public:
 	int init();
 
 	Adafruit_BMP280 bmp;
-	MPU9250 mpu;
+	bfs::Mpu9250 imu;
+
+	struct vec_struct radps;
+	struct vec_struct radps_offsets;
+	struct vec_struct mps2;
+	struct vec_struct mps2_offsets;
 
 	float temp, pressure, altitude;
-
-	float roll, pitch, yaw;
 	
 	void calibrate(float);
 	void update();
